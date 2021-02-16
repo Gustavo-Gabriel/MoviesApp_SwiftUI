edb53b3225b8d97ba69c9c3ff55224d736858b3e
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-02-16 19:46:54 -0400

    Initial Commit

---
+++ MoviesApp_SwiftUI/MoviesApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp_SwiftUIApp.swift
+//  MoviesApp_SwiftUI
+//
+//  Created by Gustavo Anjos on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
